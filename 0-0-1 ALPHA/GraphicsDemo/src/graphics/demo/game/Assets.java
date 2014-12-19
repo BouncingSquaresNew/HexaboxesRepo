011e471ef7fb99633704e671540e1485ce60f7c6
Ben Winchester <benedict.winchester@gmail.com>
2014-12-19 18:17:18 +0000

    v0.0.1 ALPHA
    I've basically got it to draw a line from the centre of the screen to
    wherever you click :)

---
+++ 0-0-1 ALPHA/GraphicsDemo/src/graphics/demo/game/Assets.java
@@ -0,0 +1,17 @@
+package graphics.demo.game;
+
+import graphics.demo.framework.Pixmap;
+import android.graphics.Typeface;
+
+
+/* Name: Assets.java
+ * Date: 23/03/2013
+ * Author: Aleksandrs Baklanovs
+ * Purpose: creates holders for all of our assets
+ */
+
+public class Assets 
+{
+	public static Typeface font;
+	
+}
