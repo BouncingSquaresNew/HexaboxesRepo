a9bd56cc386a10606346265c6b44ad24570223ea
Ben Winchester <benedict.winchester@gmail.com>
2015-01-19 13:58:17 +0000

    Attempt to program computer
    Basically, I'm trying to get the AI computer logic working, but it needs a LOT of work :D

--- 0-0-2-4-ONLINE/Computer.java
+++ 0-0-2-4-ONLINE/Computer.java
@@ -20,8 +20,63 @@ public class Computer extends User
     }
     Computer(int red, int blue, int green)
     {
-        super(red, blue, green, false, "HAL");
+        super(red, blue, green, false, "TARS");
     }
-    
+    public void makeMove(List<Hexabox> hexaboxes)
+    {
+        Hexabox hexaboxMove = hexaboxMoveCheck(hexaboxes);
+    }
+    public Hexabox hexaboxMoveCheck(List<Hexabox> hexaboxesAll)
+    {
+        List<Hexabox> = hexaboxesAll;
+        Hexabox hexaboxReturn = new Hexabox();
+        boolean hexaboxMoveFound = false;
+        int progressiveLoss = 0;
+        for(int i = 0; i < hexaboxes.size(); i++)
+        {
+            Hexabox hexaboxCurrent = hexaboxes.get(i);
+            if(hexaboxRemove.numberOfLines == 5)
+            {
+                hexaboxReturn = hexaboxCurrent;
+                hexaboxMoveFound = true;
+                break;
+            }
+        }
+        if(!hexaboxMoveFound)
+        {
+            for(int i = 0; i < hexaboxes.size(); i++)
+            {
+                Hexabox hexaboxRemove = hexaboxes.get(i);
+                if(hexaboxRemove.numberOfLines == 6)
+                {
+                    hexaboxes.remove(i); 
+                }
+            }
+            for(int i = 0; i < hexaboxes.size(); i++)
+            {
+                Hexabox hexaboxRemove = hexaboxes.get(i);
+                if(hexaboxRemove.numberOfLines == 4
+                && hexaboxes.size() > 1)
+                {
+                    hexaboxes.remove(i); 
+                }
+                else if(hexaboxRemove.numberOfLines == 4
+                && hexaboxes.size() = 1)
+                {
+                    progressiveLoss = 1;
+                }
+            }
+            switch(progressizeLoss)
+            {
+                case 0 :
+                    randomMove();
+                    break;
+                case 1 :
+                    weighUpMoves();   
+            }
+        }
+        return hexaboxReturn;
+    }
+
 }
 
